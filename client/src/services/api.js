169d5df339effdab8395cad42eb429e1f3fa4f18
Aqsa shuaib <aqsashuaib077@gmail.com>
2026-08-19 12:23:32 +0530

    Change API base URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API =axios.create({baseURL:'http://localhost:5000/api'});
+const API =axios.create({baseURL:'https://financial-analytics-backend.onrender.com'});
 API .interceptors.request.use((req)=>{
   if (localStorage.getItem('token')){
     req.headers.Authorization=`Bearer ${localStorage.getItem('token')}`;
